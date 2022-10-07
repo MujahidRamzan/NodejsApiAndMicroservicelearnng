f10698eb70461a53d9d10242607c069b436be16b
Mujahid Ramzan <mujahid.ramzan@vaimanagment.co>
2022-10-07 16:57:35 +0400

    authenticationprifle

---
+++ model/userProfile.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const userProfileSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        require: true,
+    },
+    user_fname: { type: String, default: null },
+});
+module.exports = mongoose.model("userProfile", userProfileSchema);
